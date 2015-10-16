d9333d3b7655a065cff9ad39e45356e3e4f0423f
MonKe <kevin.thommy@gmail.com>
2015-10-16 11:05:56 +0200

    port of the 'forest' test case to github pages

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 var Conf = {
-  width: 40, height: 20, // reflect any changes on book data, or maybe kaboom.
+  width: 60, height: 30, // reflect any changes on book data, or maybe kaboom.
   unit: 12,
   hitPalLength: 16, // better not touch this
   brush: [0, 1, 2],
